3289e89820c1e072a7055d5630095743a42d6263
moatasemKeliem <keliemmoatasem@gmail.com>
2026-09-13 16:04:45 +0200

    Updated from mysql to postgreSQL

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,40 +1,35 @@
-import * as dotenv from "dotenv"
-import { DataSource } from "typeorm"
-import { TrainingPlan } from "./entities/TrainingPlan"
-import { NutritionPlan } from "./entities/NutritionPlan"
-import { TrainingLog } from "./entities/TrainingLog"
-import { Article } from "./entities/Article"
-import { Subscription } from "./entities/Subscription"
-import { User } from "./entities/User"
-import fs from "fs";
+import * as dotenv from "dotenv";
+import { DataSource } from "typeorm";
+import { TrainingPlan } from "./entities/TrainingPlan";
+import { NutritionPlan } from "./entities/NutritionPlan";
+import { TrainingLog } from "./entities/TrainingLog";
+import { Article } from "./entities/Article";
+import { Subscription } from "./entities/Subscription";
+import { User } from "./entities/User";
 
-dotenv.config({ quiet: true })
+dotenv.config({ quiet: true });
+const { DATABASE_URL } = process.env;
 
-const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env
-
-if (!DB_HOST || !DB_PORT || !DB_USERNAME || DB_DATABASE == undefined) {
-    console.log(DB_HOST)
-    console.log(DB_PORT)
-    console.log(DB_USERNAME)
-    console.log(DB_PASSWORD)
-    console.log(DB_DATABASE)
-    throw new Error("Databse is missing env or isn't configured correctly")
+if (!DATABASE_URL) {
+  throw new Error("Database environment variables are missing");
 }
 
-const lokalServer = process.env.NODE_ENV === "development";
-
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: DB_HOST,
-    port: parseInt(DB_PORT),
-    username: DB_USERNAME,
-    password: DB_PASSWORD!,
-    database: DB_DATABASE,
-    synchronize: true,
-    entities: [User, TrainingPlan, NutritionPlan, TrainingLog, Article, Subscription],
-    ...(!lokalServer && {
-        ssl: {
-            ca: fs.readFileSync("/etc/secrets/ca.pem", "utf-8")
-        }
-    })
-})
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+
+  synchronize: true,
+
+  entities: [
+    User,
+    TrainingPlan,
+    NutritionPlan,
+    TrainingLog,
+    Article,
+    Subscription,
+  ],
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
